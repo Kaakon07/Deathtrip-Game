c48eac77d16e34ea59d153fc2b74209a1ed7b89e
Andybananas <83890179+Andybananas@users.noreply.github.com>
2024-02-02 13:30:52 +0100

    movement

--- Scripts/MISC/MoveScript.cs
+++ Scripts/MISC/MoveScript.cs
@@ -18,6 +18,8 @@ public class MoveScript : MonoBehaviour
     public Rigidbody2D rb;
     public Vector3 previousPlayerPosition;
 
+    public float acceleration;
+    public float deceleration;
 
     // Update is called once per frame
     void Update()
@@ -30,15 +32,15 @@ public class MoveScript : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Saves the player position before the velocity updates, this is important for chunks loading
-        previousPlayerPosition = rb.position;
-        // Definerer en Vector2 Variabel som heter movement, og den har axene du skal bevege deg i, fra -1 til 1
-        Vector2 movement = new Vector2(horizontal, vertical);
-        // Bestemmer hastigheten du beverger deg i, og direksjonen ved å gange axis inputene med bevegelses hastigheten.
-        rb.velocity = movement * MoveSpeed;
+        previousPlayerPosition = rb.position; // Saves the player position before the velocity updates, this is important for chunks loading
 
-        // bestemmer hvordan bil spriten skal rotere seg basert på veien du kjører
-        if (movement != Vector2.zero)
+        Vector2 movement = new Vector2(horizontal, vertical); // Definerer en Vector2 Variabel som heter movement, og den har axene du skal bevege deg i, fra -1 til 1
+
+        //rb.velocity = movement * MoveSpeed; // Bestemmer hastigheten du beverger deg i, og direksjonen ved å gange axis inputene med bevegelses hastigheten.
+        rb.velocity += movement * MoveSpeed*MoveSpeed * acceleration*0.02f;
+        rb.velocity -= rb.velocity * MoveSpeed * deceleration * 0.02f;
+
+        if (movement != Vector2.zero) // bestemmer hvordan bil spriten skal rotere seg basert på veien du kjører
         {
             float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
             rb.rotation = angle-90;
